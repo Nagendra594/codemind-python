b5a148b383aa836704214f9711c5a719c3d3854e
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-10-21 13:36:26 +0530

    a=int(input()) arr=list(map(int,input().split())) for i in range(a):     c=0     for j in range(a):         if arr[i]>arr[j]:             c+=1     print(c,end=' ')

--- How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -1,11 +1,9 @@
 a=int(input())
 arr=list(map(int,input().split()))
-A=[]
 for i in range(a):
     c=0
     for j in range(a):
         if arr[i]>arr[j]:
             c+=1
-    A.append(c)
-print(*A)
+    print(c,end=' ')
             
