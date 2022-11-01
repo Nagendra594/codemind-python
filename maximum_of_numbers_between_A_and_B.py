c7f212e8ac7109b77470f2c90045909e173306ae
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-01 09:04:27 +0530

    a=int(input()) ar=list(map(int,input().split())) S,E=map(int,input().split()) m=S if E>S else E M=E if m==S else S A=set() for i in ar: 	if m<=i<=M: 		A.add(i) if len(A)==0: 	print('-1') else: 	print(max(A))

---
+++ maximum_of_numbers_between_A_and_B.py
@@ -0,0 +1,13 @@
+a=int(input())
+ar=list(map(int,input().split()))
+S,E=map(int,input().split())
+m=S if E>S else E
+M=E if m==S else S
+A=set()
+for i in ar:
+	if m<=i<=M:
+		A.add(i)
+if len(A)==0:
+	print('-1')
+else:
+	print(max(A))
