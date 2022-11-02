622b4f1bdb455108ba841f42da524a2548beeb6c
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-02 20:48:28 +0530

    def dc(a):     c=0     while a:         r=a%10         c+=1         a//=10     return c a=int(input()) b=list(map(int,input().split())) C=[] for i in b:     C.append(dc(i)) print(C.count(max(C)))

---
+++ Max_digits_count.py
@@ -0,0 +1,13 @@
+def dc(a):
+    c=0
+    while a:
+        r=a%10
+        c+=1
+        a//=10
+    return c
+a=int(input())
+b=list(map(int,input().split()))
+C=[]
+for i in b:
+    C.append(dc(i))
+print(C.count(max(C)))
