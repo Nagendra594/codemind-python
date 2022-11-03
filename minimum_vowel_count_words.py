8f47388b5f619d6ef42c2ec1f2bef144b24b8645
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-03 07:01:19 +0530

    l=input() L=l.split() v=['a','e','i','o','u','A','E','I','O','U'] C=[] for i in range(len(L)): 	c=0 	for j  in L[i]: 		if j in v: 			c+=1 	C.append(c) print(C.count(min(C)))

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,11 @@
+l=input()
+L=l.split()
+v=['a','e','i','o','u','A','E','I','O','U']
+C=[]
+for i in range(len(L)):
+	c=0
+	for j  in L[i]:
+		if j in v:
+			c+=1
+	C.append(c)
+print(C.count(min(C)))
