e752276cdefe71acba4c57388879946c2e45e0e7
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-07 09:40:54 +0530

    a=input().lower() b=input().lower() v=[] for i in b: 	if i not in a and i not in v and i!=' ': 		v.append(i) for i in a: 	if i not in b and i not in v and i!=' ': 		v.append(i) print(len(v))

---
+++ Non_common_characters.py
@@ -0,0 +1,10 @@
+a=input().lower()
+b=input().lower()
+v=[]
+for i in b:
+	if i not in a and i not in v and i!=' ':
+		v.append(i)
+for i in a:
+	if i not in b and i not in v and i!=' ':
+		v.append(i)
+print(len(v))
