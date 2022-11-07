1a7158e7a02a1b5ef6f3f94f3ead44360776c621
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-07 09:18:05 +0530

    a=input().lower() b=[] for i in a: 	if i!=' ' and i not in b: 		b.append(i) print(''.join(sorted(b)))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,6 +1,6 @@
 a=input().lower()
-v=[]
+b=[]
 for i in a:
-	if i!=' ' and a.count(i)==1 and i not in v:
-		v.append(i)
-print(''.join(sorted(v)))
+	if i!=' ' and i not in b:
+		b.append(i)
+print(''.join(sorted(b)))
