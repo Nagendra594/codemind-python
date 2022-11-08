ceec78a947c742fa23232a55dcbd5a49cf161d77
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-08 20:27:55 +0530

    r,c=map(int,input().split()) S=[] for i in range(r):     arr=list(map(int,input().split()))     S.extend([arr]) s=[] for i in range(c):     ss=[]     for j in range(r):         ss.append(S[j][i])     s.append(sum(ss)) print(*s)

---
+++ Column_wise_sum_of_a_matrix.py
@@ -0,0 +1,12 @@
+r,c=map(int,input().split())
+S=[]
+for i in range(r):
+    arr=list(map(int,input().split()))
+    S.extend([arr])
+s=[]
+for i in range(c):
+    ss=[]
+    for j in range(r):
+        ss.append(S[j][i])
+    s.append(sum(ss))
+print(*s)
